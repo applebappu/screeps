bb0f74caf63a6a86461858afd2b1e99eedc17f6f
applebappu <87842348+applebappu@users.noreply.github.com>
2022-05-02 15:54:08 -0800

    setting quotas for creep types

--- main.js
+++ main.js
@@ -33,11 +33,18 @@ module.exports.loop = function () {
     }
     
     var home = Game.spawns['Spawn1']
-    if ( Object.keys(Game.creeps).length < 4 ) {
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+    var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+
+    if ( Object.keys(harvesters).length < 4 ) {
         var newName = 'Harvester ' + Game.time
         home.spawnCreep([WORK, CARRY, MOVE, MOVE], newName, { memory: { role: 'harvester'} } );
-    } else if (Object.keys(Game.creeps).length < 7 ) {
+    } else if ( Object.keys(upgraders).length < 2 ) {
         var newName = 'Upgrader ' + Game.time
         home.spawnCreep([WORK, CARRY, MOVE, MOVE], newName, { memory: { role: 'upgrader'} } );
+    } else if ( Object.keys(builders).length < 2 ) {
+        var newName = 'Builder ' + Game.time
+        home.spawnCreep([WORK, CARRY, MOVE, MOVE], newName, { memory: { role: 'builder'} } );
     }
 };
