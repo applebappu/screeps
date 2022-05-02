6a4aab85d61352546ff8e523b919c325d5ffc90b
applebappu <87842348+applebappu@users.noreply.github.com>
2022-05-02 15:44:42 -0800

    big fixes to basic functions

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,28 +1,29 @@
 var roleUpgrader = {
-	/** @param {Creep} creep **/
-	run: function(creep) {
-		if ( creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0 ) {
-			creep.memory.upgrading = false;
-		} else if ( !creep.memory.upgrading && creep.store.getFreeCapacity() == 0 ) {
-			creep.memory.upgrading = true;
-		}
 
-		if ( creep.memory.upgrading ) {
-			if ( creep.upgradeController(creep.room.controller ) == ERR_NOT_IN_RANGE ) {
-				creep.moveTo(creep.room.controller);
-			} else {
-				var droppedEnergy = creep.room.find(FIND_DROPPED_RESOURCES, {
-					filter: resource => resource.resourceType == RESOURCE_ENERGY
-				})
+    /** @param {Creep} creep **/
+    run: function(creep) {
 
-				var closestDroppedEnergy = creep.pos.findClosestByPath(droppedEnergy)
+        if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.upgrading = false;
+            creep.say('🔄 harvest');
+        }
+        if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
+            creep.memory.upgrading = true;
+            creep.say('⚡ upgrade');
+        }
 
-				if ( creep.pickup(closestDroppedEnergy) == ERR_NOT_IN_RANGE ) {
-					creep.moveTo(closestDroppedEnergy);
-				}
-			}
-		}
-	}
+        if(creep.memory.upgrading) {
+            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+            }
+        }
+        else {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
+    }
 };
 
 module.exports = roleUpgrader;
