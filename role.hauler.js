1d758925380a09bae0486664b85bf8f84792f12a
applebappu <87842348+applebappu@users.noreply.github.com>
2022-05-02 14:59:59 -0800

    fixing logic

--- role.hauler.js
+++ role.hauler.js
@@ -1,24 +1,31 @@
 var roleHauler = {
-	/** @param {Creep} creep **/
-	run: function(creep) {
-		if ( creep.store.getFreeCapacity() > 0 ) {
-			const droppedEnergy = creep.room.find(FIND_DROPPED_RESOURCES, {
-				filter: resource => resource.resourceType == RESOURCE_ENERGY
-			})
-			const closestDroppedEnergy = creep.pos.findClosestByPath(droppedEnergy)
-
-			if ( creep.pickup(closestDroppedEnergy) == ERR_NOT_IN_RANGE ) {
-				creep.moveTo(closestDroppedEnergy);
-			}
-		} else {
-			const spawns = creep.room.find(FIND_MY_SPAWNS)
-			const closestSpawn = creep.pos.findClosestByPath(spawns)
-
-			if ( creep.transfer(closestSpawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-				creep.moveTo(closestSpawn);
-			}
-		}
-	}
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        if ( creep.store.getFreeCapacity() > 0 ) {
+            var droppedEnergy = creep.room.find(FIND_DROPPED_RESOURCES, {
+                filter: resource => resource.resourceType == RESOURCE_ENERGY
+            })
+            var closestDroppedEnergy = creep.pos.findClosestByPath(droppedEnergy)
+            
+            if ( creep.pickup(closestDroppedEnergy) == ERR_NOT_IN_RANGE ) {
+                creep.moveTo(closestDroppedEnergy);
+            }
+        } else {
+            var buildSite = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+            if ( buildSite ) {
+                if ( creep.build(buildSite) == ERR_NOT_IN_RANGE ) {
+                    moveTo(buildSite);
+                }
+            } else {
+                var mySpawns = creep.room.find(FIND_MY_SPAWNS)
+                var closestSpawn = creep.pos.findClosestByPath(mySpawns)
+                
+                if ( creep.transfer(closestSpawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ) {
+                    creep.moveTo(closestSpawn);
+                }
+            }
+        }
+    }
 };
 
 module.exports = roleHauler;
