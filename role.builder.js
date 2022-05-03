ffe9093305f1f7e222b4ff0ddca98736b293ea30
applebappu <87842348+applebappu@users.noreply.github.com>
2022-05-02 16:15:18 -0800

    quick and dirty closest source pathfinding

--- role.builder.js
+++ role.builder.js
@@ -16,14 +16,16 @@ var roleBuilder = {
             var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
             if(targets.length) {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } } );
                 }
             }
         }
         else {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            var closestSource = creep.pos.findClosestByPath(sources)
+            
+            if(creep.harvest(closestSource) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(closestSource, { visualizePathStyle: { stroke: '#ffaa00' } } );
             }
         }
     }
