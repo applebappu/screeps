1e12b2f8b58cf51905732e4d1e0c998b2b136743
applebappu <87842348+applebappu@users.noreply.github.com>
2022-05-02 16:13:53 -0800

    Create role.defender.js

---
+++ role.defender.js
@@ -0,0 +1,11 @@
+var roleDefender = {
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        var closestHostile = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if ( closestHostile ) {
+            creep.attack(closestHostile);
+        }
+    }
+};
+
+module.exports = roleDefender;
