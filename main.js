f63371914131aacf800521d7be58f94db8238baa
applebappu <87842348+applebappu@users.noreply.github.com>
2022-05-02 16:13:25 -0800

    implemented Defenders

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
+var roleDefender = require('role.defender');
 
 module.exports.loop = function () {
 
@@ -19,32 +20,42 @@ module.exports.loop = function () {
         }
     }
 
-    for(var name in Game.creeps) {
+    for( var name in Game.creeps ) {
         var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
+        if( creep.memory.role == 'harvester' ) {
             roleHarvester.run(creep);
         }
-        if(creep.memory.role == 'upgrader') {
+        if( creep.memory.role == 'upgrader' ) {
             roleUpgrader.run(creep);
         }
-        if(creep.memory.role == 'builder') {
+        if( creep.memory.role == 'builder' ) {
             roleBuilder.run(creep);
+        }
+        if ( creep.memory.role == 'defender' ) {
+            roleDefender.run(creep);
         }
     }
     
-    var home = Game.spawns['Spawn1']
+    var home = Game.spawns['Spawn1'];
+    var workerSpec = [WORK, CARRY, MOVE, MOVE];
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-
+    
+    var defenderSpec = [TOUGH, TOUGH, TOUGH, TOUGH, ATTACK, MOVE, ATTACK, MOVE];
+    var defenders = _.filter(Game.creeps, (creep) => creep.memory.role == 'defender');
+    
     if ( Object.keys(harvesters).length < 4 ) {
         var newName = 'Harvester ' + Game.time
-        home.spawnCreep([WORK, CARRY, MOVE, MOVE], newName, { memory: { role: 'harvester'} } );
+        home.spawnCreep(workerSpec, newName, { memory: { role: 'harvester' } } );
     } else if ( Object.keys(upgraders).length < 2 ) {
         var newName = 'Upgrader ' + Game.time
-        home.spawnCreep([WORK, CARRY, MOVE, MOVE], newName, { memory: { role: 'upgrader'} } );
+        home.spawnCreep(workerSpec, newName, { memory: { role: 'upgrader' } } );
     } else if ( Object.keys(builders).length < 2 ) {
         var newName = 'Builder ' + Game.time
-        home.spawnCreep([WORK, CARRY, MOVE, MOVE], newName, { memory: { role: 'builder'} } );
+        home.spawnCreep(workerSpec, newName, { memory: { role: 'builder' } } );
+    } else if ( Object.keys(defenders).length < 2 ) {
+        var newName = 'Defender ' + Game.time
+        home.spawnCreep(defenderSpec, newName, { memory: { role: 'defender' } } );
     }
 };
