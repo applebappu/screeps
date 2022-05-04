5cc20a9561dddc9d056ce5cba342753e9cc08c4d
applebappu <87842348+applebappu@users.noreply.github.com>
2022-05-04 10:06:27 -0800

    testing dynamic run function naming

--- main.js
+++ main.js
@@ -1,39 +1,14 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
-var roleDefender = require('role.defender');
+var roleharvester = require('role.harvester');
+var roleupgrader = require('role.upgrader');
+var rolebuilder = require('role.builder');
+var roledefender = require('role.defender');
+var rolehauler = require('role.hauler');
 
 module.exports.loop = function () {
-
-    var tower = Game.getObjectById('TOWER_ID');
-    if(tower) {
-        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
-        });
-        if(closestDamagedStructure) {
-            tower.repair(closestDamagedStructure);
-        }
-
-        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if(closestHostile) {
-            tower.attack(closestHostile);
-        }
-    }
-
     for( var name in Game.creeps ) {
-        var creep = Game.creeps[name];
-        if( creep.memory.role == 'harvester' ) {
-            roleHarvester.run(creep);
-        }
-        if( creep.memory.role == 'upgrader' ) {
-            roleUpgrader.run(creep);
-        }
-        if( creep.memory.role == 'builder' ) {
-            roleBuilder.run(creep);
-        }
-        if ( creep.memory.role == 'defender' ) {
-            roleDefender.run(creep);
-        }
+        var role = creep.memory.role;
+        var runfunc = str.concat('role', role);
+        runfunc.run(creep);
     }
     
     var home = Game.spawns['Spawn1'];
