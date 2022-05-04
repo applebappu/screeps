4314e41d9acfc2915319f8c54136e5815752ccd0
applebappu <87842348+applebappu@users.noreply.github.com>
2022-05-04 09:58:40 -0800

    minor formatting changes, removed say

--- role.builder.js
+++ role.builder.js
@@ -3,19 +3,17 @@ var roleBuilder = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
-        if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+        if ( creep.memory.building && creep.store[RESOURCE_ENERGY] == 0 ) {
             creep.memory.building = false;
-            creep.say('🔄 harvest');
         }
-        if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+        if ( !creep.memory.building && creep.store.getFreeCapacity() == 0 ) {
             creep.memory.building = true;
-            creep.say('🚧 build');
         }
 
-        if(creep.memory.building) {
+        if ( creep.memory.building ) {
             var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+            if ( targets.length ) {
+                if ( creep.build(targets[0]) == ERR_NOT_IN_RANGE ) {
                     creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } } );
                 }
             }
@@ -24,7 +22,7 @@ var roleBuilder = {
             var sources = creep.room.find(FIND_SOURCES);
             var closestSource = creep.pos.findClosestByPath(sources)
             
-            if(creep.harvest(closestSource) == ERR_NOT_IN_RANGE) {
+            if ( creep.harvest(closestSource) == ERR_NOT_IN_RANGE ) {
                 creep.moveTo(closestSource, { visualizePathStyle: { stroke: '#ffaa00' } } );
             }
         }
